db1718047d1e5a63d9de59e5c71f6dd76f1ba762
Nicolás Horro <nhorro@gmail.com>
2020-07-09 23:51:24 -0300

    práctico Intro a IA clase 2

--- intro_ai/clase_1/ex1_norm.py
+++ intro_ai/clase_1/ex1_norm.py
@@ -5,15 +5,12 @@ def vector_norm_l0(matrix):
     mask = matrix > 0
     return np.sum(mask, axis=1)
 
-
 def vector_norm_l1(matrix):
     abs_m = np.abs(matrix)
     return np.sum(abs_m, axis=1)
 
-
 def vector_norm_l2(matrix):
     return np.sqrt(np.sum(matrix ** 2, axis=1))
-
 
 def vector_norm_inf(m):
     return np.max(m, axis=1)
