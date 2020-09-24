23587b89979353aa525bb25371fb846b11cfdb5f
Nicolás Horro <nhorro@gmail.com>
2020-09-23 22:59:40 -0300

    corregido métricas y función de costo

--- intro_ai/libintroia/utils.py
+++ intro_ai/libintroia/utils.py
@@ -9,14 +9,16 @@ def plot_confusion_matrix(predicted,actual,title="Confusion Matrix"):
     FN = np.count_nonzero( (predicted != actual) & (predicted==False))
     data = np.array([
         [TP,FP],
-        [TN,FN]
+        [FN,TN]
     ])
     seaborn.set(color_codes=True)
     plt.figure(1, figsize=(9, 6)) 
     plt.title(title) 
-    labels=["True","False","True","False"]
+
     seaborn.set(font_scale=1.4)
     ax = seaborn.heatmap(data, annot=True, cmap="YlGnBu", cbar_kws={'label': 'Scale'}, fmt='g')
-    ax.set_xticklabels(labels)
-    ax.set_yticklabels(labels) 
-    ax.set(ylabel="True Label", xlabel="Predicted Label")
+    ax.set_yticklabels(["True","False"])
+    ax.set_xticklabels(["True","False"])
+
+    ax.set(ylabel="Predicted Label", xlabel="Actual Label")
+    return TP,TN,FP,FN
